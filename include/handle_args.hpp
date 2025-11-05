8bd14c57bb15b9ebf78a79f4fd67d60fd978f492
gfaerny <gfaerny@gmail.com>
2025-11-05 22:12:34 +0330

    nothing specific

---
+++ include/handle_args.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <cerrno>
+#include <csignal>
+#include <iostream>
+#include <stdatomic.h>
+#include <vector>
+#define texit return 0;
--- include/tng_make_file.hpp
+++ include/tng_make_file.hpp
@@ -1,11 +1,16 @@
 #pragma once
 #include <vector>
 #include <string>
-#include <fstream>
-#include <filesystem>
-#include <iostream>
+#include <print>
 #include <pwd.h>
 #include <unistd.h>
 #include <sys/types.h>
+
+#include <cstdlib>
+#include <cstring>
+#include <filesystem>
+#include <fstream>
+#include <streambuf>
+
 
 void tng_make_file(const std::vector<std::string> , const bool TNG_CONFIG , const bool TNG_LICENSE);
