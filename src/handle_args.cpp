0615b04bdc52a473cdb53ebf2b2ac19aa3d9dd3e
Iliya Abolghasemi <gfaerny@gmail.com>
2026-07-29 15:47:04 +0330

    handle_args: no need for -l && --license flag because of new config system

--- src/handle_args.cpp
+++ src/handle_args.cpp
@@ -1,24 +1,23 @@
 #include "../include/handle_args.hpp"
+#include <vector>
 
 uint16_t iit = 0;
 
-bool license_bool{NO}, config_bool{NO};
+bool license_called{NO}, config_called{NO};
 
-void handle_args(std::vector<std::string> &tng_args_vec)
+auto handle_args(std::vector<std::string> &tng_args_vec) -> void
 {
     for (auto it : tng_args_vec)
     {
-        ++iit;
+        iit++;
         // Help
         if (it == "-h" || it == "--help")
         {
-            std::printf(
-                "tng usage : tng -[OPTIONS] [OPTION_CONFIG_NAME] [FILENAME]\n"
+            std::printf("tng usage : tng -[OPTIONS] [OPTION_CONFIG_NAME] [FILENAME]\n"
 
-                "-c    or    --config  :   use your writed config's file for adding it to files that going to "
-                "created\n\n\n"
-                "-l    or    --license :   use you writted or available license for adding them to you file\n\n\n"
-                "-h    or    --help    :   use it to see this massage and tng gotta exit\n");
+                        "-c    or    --config  :   use your writed config file for adding it to files that going to "
+                        "created\n\n\n"
+                        "-h    or    --help    :   use it to see this massage and tng gotta exit\n");
             return;
         }
         // Config
@@ -30,62 +29,24 @@ void handle_args(std::vector<std::string> &tng_args_vec)
                 std::printf("tng error : No config file selected\nuse --help or -h option's to see usage");
                 throw tng_error{.error_type_o = error_type::c_no_config_file_select, .error_massage = {}};
             }
-            else if (config_bool == YES)
+            else if (config_called == YES)
             {
                 std::printf("tng error : You can't address cofing file more then one time per command");
                 throw tng_error{.error_type_o = error_type::c_cant_select_multi_conf, .error_massage = {}};
             }
             else
             {
+                // Remove argument contian config path and auto flag -> config
                 tng_args_vec.erase(tng_args_vec.begin() + iit);
-                config_bool = YES;
+                tng_args_vec.erase(tng_args_vec.begin() + (iit + 1));
+                config_called = YES;
 
-                // Config_path is global var and it's effort config class when try to load config
+                // Config_path is global var and it's effort config class when try to auto config -> load
+                // TODO: write handler auto use -> for
                 config_path = tng_args_vec[iit];
             }
         }
-        // License
-        else if (it == "-l" || it == "--license")
-        {
-            // TODO: need to handle
-            // tng file.x -l
-            if (tng_args_vec.at(iit).empty())
-            {
-                std::printf("tng error : No license file selected\nuse --help or -h option's for see usage");
-                throw tng_error{.error_type_o = error_type::arg_expected_argument, .error_massage = {}};
-            }
-            else if (license_bool == YES)
-            {
-                std::printf("tng error : You can't address license file more then one time per command, but you  can "
-                            "use .config file for setting license PATH's that can help you find you license when it is "
-                            "not in this directory\n");
-                // throw tng_error{.error_type = ""};
-            }
-            else
-            {
-                license_filename = tng_args_vec[iit];
-                tng_args_vec.erase(tng_args_vec.begin() + iit);
-                license_bool = YES;
-            }
-        }
     }
 
-    // Call tng_make_file for pass config_path and license_name
-    if (config_bool && license_bool)
-    {
-        tng_make_file(tng_args_vec, &config_path, &license_filename);
-    }
-    else if (!config_bool && license_bool)
-    {
-        tng_make_file(tng_args_vec, NULL, &license_filename);
-    }
-    else
-    {
-        tng_make_file(tng_args_vec, NULL, NULL);
-    }
-
-    // We have license_filename
-    // We have cofnig_path
-    // We have all files name in tng_args_vec
-    return;
+    tng_make_file(std::move(tng_args_vec), &config_path);
 }
