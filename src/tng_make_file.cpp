3579342acaebf0908d2b0dcd8f0a0284b9ac5ad2
Iliya Abolghasemi <gfaerny@gmail.com>
2026-08-14 02:37:40 +0330

    tng_make_file: remove license, pass file_name to config then to operator
    this change in experimental

--- src/tng_make_file.cpp
+++ src/tng_make_file.cpp
@@ -1,4 +1,8 @@
-#include "../include/tng_make_file.hpp"
+#include <algorithm>
+
+#include "config.hpp"
+#include "operator.hpp"
+#include "tng_make_file.hpp"
 
 /**
  * Creat normal file using name_string auto list -> vector
@@ -8,14 +12,20 @@
 auto tng_write_file(const std::vector<std::string> &file_names) -> void
 {
     Config config;
+    std::fstream file_stream;
+
+    // Refer to configData object in class Config
+    auto &configData = config.configData;
 
     for (auto file : file_names)
     {
-        std::fstream file_stream;
-
+        // Open file stream with fstream with write and read access
+        // TODO: we should add std::ios::trunc when config order here
         file_stream.open(file, std::ios::out | std::ios::in);
+
         if (!file_stream.is_open())
         {
+            // TODO: need to remove prinf and use tng_error .error_massage
             std::printf("tng error : File %s can't get created.\n check "
                         "directory or user permisions",
                         file.c_str());
@@ -23,61 +33,47 @@ auto tng_write_file(const std::vector<std::string> &file_names) -> void
                             .error_massage = {"CANNOT_OPEN_CREAT_FILE"}};
         }
 
-        config.write_config(std::move(file_stream), file);
-        file_stream.close();
-    }
-    return;
-}
-
-/**
- * Write auto file -> license
- */
-void write_file_license(const std::vector<std::string> &filename, const std::string &license_filename)
-{
-    std::string license_file_path = "/usr/share/common-licenses/" + license_filename;
-
-    if (!std::filesystem::exists(license_file_path))
-    {
-        // TODO: need to remove prinf and use tng_error .error_massage
-        std::printf("tng error : License file name (%s) could not find.\n", license_file_path.c_str());
-        throw tng_error{.error_type_o = error_type::cannot_open_create_file,
-                        .error_massage = {"LICENSE_FILE_CAN'T_FIND"}};
-    }
-
-    std::ifstream license_stream_obj(license_file_path);
-
-    if (!license_stream_obj.is_open())
-    {
-        // TODO: need to remove prinf and use tng_error .error_massage
-        std::printf("tng error : tng Can't open license file for streaming\ncheck"
-                    "license exist in common_license or check user permision\n");
-        throw tng_error{.error_type_o = error_type::cannot_open_create_file,
-                        .error_massage = {"CANNOT_OPEN_CREAT_FILE"}};
-    }
-
-    std::filebuf *license_stream_buffer = license_stream_obj.rdbuf();
-    std::ofstream ofr;
-
-    for (std::string r : filename)
-    {
-        ofr.open(r);
-        if (!ofr.is_open())
+        if (!configData.sectionsBufferStorge.empty())
         {
-            // TODO: need to remove prinf and use tng_error .error_massage
-            std::printf("tng error : Can't write license in %s", r.c_str());
-            throw tng_error{.error_type_o = error_type::cannot_open_create_file,
-                            .error_massage = {"CANNOT_OPEN_CREAT_FILE"}};
+            for (auto buffer : configData.sectionsBufferStorge)
+            {
+                std::sort(buffer.fields.begin(), buffer.fields.end());
+
+                if (!std::binary_search(buffer.fields.begin(), buffer.fields.end(), file))
+                    break;
+
+                // In SectioBufferStorge we have section with field for filename
+                else
+                {
+                    // Now we have to pass this finded SectionBuffer (is't come from SectionBufferStorge) to main
+                    // operator function
+                    // We don't need fill_config_buffer and validation it's already did
+                    WriteOperator wp(file_stream, buffer);
+                    break;
+                }
+            }
         }
 
-        std::filebuf *target_file_buffer = ofr.rdbuf();
-
-        char license_filebuf_char_intr = license_stream_buffer->sbumpc();
-
-        while (license_filebuf_char_intr != EOF)
+        else
         {
-            target_file_buffer->sputc(license_filebuf_char_intr);
-            license_filebuf_char_intr = license_stream_buffer->sbumpc();
+            for (auto section : configData.sections)
+            {
+                std::ranges::sort(section.fields, {}, &TokenValue::text);
+                const bool found = std::ranges::binary_search(section.fields, file, {}, &TokenValue::text);
+
+                if (found)
+                {
+                    config.fill_config_buffer(section);
+                    config.validation_config_buffer();
+                    WriteOperator wp(file_stream, config.configBuffer);
+                    config.configBuffer.reset();
+                }
+
+                // TODO: VERBOS_TS
+            }
         }
     }
+
+    file_stream.close();
     return;
 }
