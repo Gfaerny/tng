05ddad25a3d90ec9f8387336d66b815825d22988
Iliya Abolghasemi <gfaerny@gmail.com>
2026-09-03 12:12:31 +0330

    config: change #pragma to regular ifndef: more compiler support

--- include/buffer.hpp
+++ include/buffer.hpp
@@ -1,20 +1,26 @@
-#pragma once
+#ifndef BUFFER_H
+#define BUFFER_H
 
 #include <optional>
 #include <string>
+#include <string_view>
 #include <vector>
 
 #include "macro.h"
 
 struct SectionBuffer
 {
-    std::vector<std::string> fields{};
-    std::optional<bool> comment = std::nullopt;
-
     // `comment_style` vairable get filled in config file with `block` or `line` option
     // and in this buffer comment_style in order can be {YES} -> `block` and {NO} -> `line`
+
+    std::vector<std::string_view> fields{};
+    std::vector<std::string_view> header_texts{};
+    std::vector<std::string_view> footer_texts{};
+
+    std::optional<bool> comment = std::nullopt;
     std::optional<bool> comment_style;
-    std::optional<bool> overwrite_existing_file;
+    std::optional<bool> include_license;
+
     std::string_view line_prefix{""};
     std::string_view block_header{""};
     std::string_view block_line_prefix{""};
@@ -22,16 +28,14 @@ struct SectionBuffer
     std::string license_path{""};
 
     // preconfiged
+    bool overwrite_existing_file{NO};
     int space_between_header_footer{4};
     bool include_license_before_header{YES};
 
     // Layout
     // TODO: check if don't need use _view sufrix for layout value
-    std::string header_text{""};
-    std::string file_introduce{""};
-    std::string time_introduce{""};
-    std::string license_introduce{""};
-    std::string footer_text{""};
 
     auto reset() -> void;
 };
+
+#endif
