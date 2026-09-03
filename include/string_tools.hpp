d18ecd4af9f74260a007e8ec1e57da544696c2a0
Iliya Abolghasemi <gfaerny@gmail.com>
2026-09-03 12:18:17 +0330

    overall: change function define type

--- include/string_tools.hpp
+++ include/string_tools.hpp
@@ -1,17 +1,22 @@
-#pragma once
+#ifndef STRING_TOOLS_H
+#define STRING_TOOLS_H
 
 #include <optional>
 #include <string>
 
 namespace StringTools
 {
-std::basic_string<char> extension_type(const std::string &filename);
+auto extension_type(const std::string &filename) -> std::basic_string<char>;
 
-std::string clear_char_space(std::string &string);
+auto clear_char_space(std::string &string) -> std::string;
 
 std::string after_until(const std::string &string, const char &after, const char &until);
 
 template <typename T> T after_eqaul_value(std::string &line);
 
 std::optional<int> string_line_len(const std::string &string);
+
+auto string_to_int(const std::string &string) -> int;
 }; // namespace StringTools
+
+#endif
