7814ce0390b6d290e92fa18c87508ec9ab679e78
Iliya Abolghasemi <gfaerny@gmail.com>
2026-09-03 12:10:52 +0330

    config: change access status for HOME var
    this variable needed to be inline for more use and no defination confilict in
    compile time

--- include/config.hpp
+++ include/config.hpp
@@ -1,72 +1,26 @@
-#pragma once
+#ifndef CONFIG_H
+#define CONFIG_H
 
-#include <fstream>
+#include <filesystem>
 #include <pwd.h>
 #include <unistd.h>
 #include <vector>
 
 #include "buffer.hpp"
+#include "macro.h"
+#include "search.hpp"
+#include "section.hpp"
 
-passwd *pw = getpwuid(getuid());
-char *c_style_home_dir = pw->pw_dir;
+inline passwd *pw = getpwuid(getuid());
+inline char *PWD = pw->pw_dir;
 
-inline std::string home_dir_path = c_style_home_dir;
-inline std::string config_path = home_dir_path + "/.config/tng/tng.conf";
-
-struct MetaData
-{
-    size_t line{}, column;
-};
-
-struct TokenValue
-{
-    std::string text{};
-    MetaData metadata;
-
-    auto set_text(std::string txt) -> void;
-    auto set_metadata(MetaData md) -> void;
-
-    auto get_metadata() const -> MetaData;
-    auto get_value() const -> std::string;
-};
-
-struct VariableValue
-{
-    TokenValue variable;
-    TokenValue value;
-
-    /*
-     *
-     */
-    auto push_metadata(size_t line, size_t column, bool var_val) -> void;
-    /*
-     * return metadata if bool is 0 return variable.metadata
-     */
-    MetaData get_metadata(bool is_variable) const;
-    std::string_view get_variable() const;
-    std::string_view get_value() const;
-};
-
-struct Section
-{
-    std::vector<TokenValue> fields{};
-    std::vector<VariableValue> variable_value{};
-
-    /*
-     * Push in order fields
-     */
-    auto push_field(const std::string fieldStr, const size_t line, const size_t column);
-
-    /*
-     * Push in order, variavble and value string with bool true or false value
-     */
-    auto push_var_val(const std::string variableOrvalue, const bool is_variable, const size_t line, const size_t column)
-        -> void;
-};
+inline fs::path home_directory{std::string(PWD)};
+inline fs::path config_path = Search::base_project_dir().value();
 
 struct ConfigData
 {
     int current_index{0};
+    // All of config file gather around in sections vector
     std::vector<Section> sections{};
     std::vector<SectionBuffer> sectionsBufferStorge{};
 
@@ -80,14 +34,10 @@ class Config
   public:
     ConfigData configData;
     SectionBuffer configBuffer;
-    std::vector<SectionBuffer> readed_config_section_buffer;
 
     Config();
 
     auto fill_config_buffer(const Section &section) -> void;
     auto validation_config_buffer() -> void;
-    auto write_config(const std::fstream &file_stream, const std::string_view &file_name) -> void;
-
-  protected:
-    // All values
 };
+#endif
