5da7ffaa4691b847f6ba75dfc2d3d5139adbda2e
Iliya Abolghasemi <gfaerny@gmail.com>
2026-09-03 12:14:34 +0330

    handle_args: use std::filesystem::path as file passing and add needed handle function for it

--- include/handle_args.hpp
+++ include/handle_args.hpp
@@ -1,13 +1,14 @@
-#pragma once
+#ifndef HANDLE_ARGS_H
+#define HANDLE_ARGS_H
 
-#include <cerrno>
-#include <csignal>
-#include <cstdio>
-#include <filesystem>
+#include <string>
 #include <vector>
 
-#include "config.hpp"
-#include "error.hpp"
-#include "tng_make_file.hpp"
+#include "macro.h"
 
-void handle_args(std::vector<std::string> &tng_args_vec);
+static auto expand_tilde(const fs::path &input_path) -> fs::path;
+auto resolve_to_absolute(const fs::path &raw_path) -> fs::path;
+auto handle_file(std::vector<std::string> file_explanations) -> std::vector<fs::path>;
+auto handle_args(std::vector<std::string> &tng_args_vec) -> void;
+
+#endif
