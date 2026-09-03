fff7e1c981f9a164e210ffdb07c5b3173ba9f6c7
Iliya Abolghasemi <gfaerny@gmail.com>
2026-09-03 12:17:51 +0330

    write_operator: drop refrence var and define API style for operator class usage

--- include/operator.hpp
+++ include/operator.hpp
@@ -1,22 +1,28 @@
-#pragma once
+#ifndef OPERATOR_H
+#define OPERATOR_H
 
 #include <cstdio>
 #include <fstream>
+#include <optional>
+#include <string_view>
 
 #include "buffer.hpp"
 
 class WriteOperator
 {
   public:
-    explicit WriteOperator(std::fstream &file_stream, SectionBuffer &section_buffer);
+    explicit WriteOperator(fs::path file, SectionBuffer &section_buffer, bool overwrite_file);
+    explicit WriteOperator(fs::path file, bool overwrite_file);
 
     auto import_text(const std::string &text) -> void;
     auto import_header_text() -> void;
     auto import_license_text() -> void;
     auto import_footer_text() -> void;
+    auto import_space_b_header_footer() -> void;
 
   private:
-    SectionBuffer section_buffer_copy;
-    std::fstream file_stream_copy;
-    std::string line_comment_spec = {""};
+    SectionBuffer section_buffer{};
+    std::fstream file_stream;
 };
+
+#endif
--- src/operator.cpp
+++ src/operator.cpp
@@ -1,89 +1,79 @@
-#include <fstream>
+#include <cstddef>
+#include <cstdlib>
 #include <optional>
 #include <sstream>
-#include <streambuf>
-#include <vector>
 
+#include "buffer.hpp"
+#include "macro.h"
 #include "operator.hpp"
-#include "string_tools.hpp"
 
 auto WriteOperator::import_text(const std::string &text) -> void
 {
     std::vector<std::string> lines_header_text{};
-
+    std::istringstream input(text);
     std::string line{};
-    for (const auto &ch : text)
+    while (std::getline(input, line))
     {
-        if (ch == '\n')
-        {
-            lines_header_text.push_back(line);
-            line.clear();
-        }
-        line += ch;
+        lines_header_text.push_back(line);
     }
 
-    if (section_buffer_copy.comment == YES)
+    if (section_buffer.comment == YES)
     {
-        if (section_buffer_copy.comment_style == YES)
+        if (section_buffer.comment_style == YES)
         {
             if (lines_header_text.size() >= 3)
             {
                 // seperate header_string by line and push to lines_header_text vector
-
-                for (size_t iit = 0; iit - 2 < lines_header_text.size(); ++iit)
+                file_stream << section_buffer.block_header << '\n';
+                for (auto &line_input : lines_header_text)
                 {
-                    if (iit == 1)
-                    {
-                        file_stream_copy << section_buffer_copy.block_header << '\n';
-                    }
-                    else if (static_cast<size_t>(iit + 1) == lines_header_text.size())
-                    {
-                        file_stream_copy << section_buffer_copy.block_footer << '\n';
-                    }
-                    else
-                    {
-                        file_stream_copy << section_buffer_copy.block_line_prefix << " " << lines_header_text[iit - 2]
-                                         << '\n';
-                    }
+                    file_stream << section_buffer.block_line_prefix << " " << line_input << '\n';
                 }
+                file_stream << section_buffer.block_footer << '\n';
             }
             else
             {
                 // throw error: we need 3 or more line to use Block style
             }
         }
-        else if (section_buffer_copy.comment_style == NO)
+        else if (section_buffer.comment_style == NO)
         {
             for (size_t iit = 0; iit < lines_header_text.size(); ++iit)
             {
-                file_stream_copy << section_buffer_copy.line_prefix;
-                file_stream_copy << lines_header_text[iit] << '\n';
+                file_stream << section_buffer.line_prefix;
+                file_stream << lines_header_text[iit] << '\n';
             }
         }
     }
 
-    else if (section_buffer_copy.comment == NO)
+    else if (section_buffer.comment == NO)
     {
         for (size_t iit = 0; iit < lines_header_text.size(); iit++)
         {
-            file_stream_copy << lines_header_text[iit] << '\n';
+            file_stream << lines_header_text[iit] << '\n';
         }
     }
 }
 
 auto WriteOperator::import_header_text() -> void
 {
-    import_text(section_buffer_copy.header_text);
+    for (const auto &line : section_buffer.header_texts)
+    {
+        import_text(std::string(line));
+    }
 }
 
 auto WriteOperator::import_footer_text() -> void
 {
-    import_text(section_buffer_copy.footer_text);
+    for (const auto &line : section_buffer.footer_texts)
+    {
+        import_text(std::string(line));
+    }
 }
 
 auto WriteOperator::import_license_text() -> void
 {
-    std::fstream license_stream(section_buffer_copy.license_path);
+    std::fstream license_stream(section_buffer.license_path);
     if (!license_stream.is_open())
     {
         // throw error : problem in opening license file
@@ -99,14 +89,50 @@ auto WriteOperator::import_license_text() -> void
     license_stream.close();
 }
 
-WriteOperator::WriteOperator(std::fstream &file_stream, SectionBuffer &section_buffer)
+auto WriteOperator::import_space_b_header_footer() -> void
 {
-    if (!file_stream.is_open())
+    for (int count{0}; count < section_buffer.space_between_header_footer; count++)
     {
-        // throw error
+        import_text("\n");
     }
+}
 
-    // Swap file stream ans section config buffer resource
-    file_stream_copy.swap(file_stream);
-    std::swap(section_buffer, section_buffer_copy);
+WriteOperator::WriteOperator(fs::path file, SectionBuffer &section_buffer, bool overwrite_file)
+{
+    if (overwrite_file)
+        file_stream.open(file, std::ios::in | std::ios::out | std::ios::trunc);
+    else
+        file_stream.open(file, std::ios::in | std::ios::out);
+
+    // Ask field config for add license before or not
+    if (section_buffer.include_license_before_header)
+        goto HEADER_FIRST;
+
+    else
+        goto LICENSE_FIRST;
+
+HEADER_FIRST:
+    import_header_text();
+    import_license_text();
+    import_space_b_header_footer();
+    import_footer_text();
+    goto END;
+
+LICENSE_FIRST:
+    import_license_text();
+    import_header_text();
+    import_space_b_header_footer();
+    import_footer_text();
+
+END:
+    file_stream.close();
+}
+WriteOperator::WriteOperator(fs::path file, bool overwrite_file)
+{
+    if (overwrite_file)
+        file_stream.open(file, std::ios::in | std::ios::out | std::ios::trunc);
+    else
+        file_stream.open(file, std::ios::in | std::ios::out);
+
+    file_stream.close();
 }
